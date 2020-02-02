b83503b0ca5bea720591abb7adcdd060e54f91dd
Carter Snook <cartersnook04@gmail.com>
2020-02-02 12:06:39 -0600

    Create manipulating-dictionaries.py

---
+++ 2.2/manipulating-dictionaries.py
@@ -0,0 +1,11 @@
+# A Dictionary is an unordered set of data
+definitions = {
+    "Noun": [
+        "to score a point as in Baseball"
+    ],
+    "Verb": [
+        "to move the legs quickly"
+    ]
+}
+
+print(definitions["Noun"][0])
