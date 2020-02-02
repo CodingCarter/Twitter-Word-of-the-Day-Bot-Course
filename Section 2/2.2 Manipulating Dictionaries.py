417ad8a35a6029754af994e22fba6baf5b7606d1
Carter Snook <cartersnook04@gmail.com>
2020-02-02 12:04:44 -0600

    Add files via upload

---
+++ Section 2/2.2 Manipulating Dictionaries.py
@@ -0,0 +1,11 @@
+# A Dictionary is an unordered set of data
+definitions = {
+    "Noun": [
+        "a person"
+    ],
+    "Verb": [
+        "someone who angers currently"
+    ]
+}
+
+print(definitions["Noun"][0])
